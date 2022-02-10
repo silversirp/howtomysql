a78c28d0cac3421ac0261433dc41a7744b75162c
Silver Sirp <silver.sirp@khk.ee>
2022-02-11 00:42:35 +0200

    Insert into

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ con.connect(function (err){
     if (err) throw err;
     console.log("Connected!");
 
-    var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
 
     con.query(sql, function (err, result) {
         if(err) throw err;
