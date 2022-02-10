3eb5c8a44458c56ba3dc7b04bdc22df77227c4da
Silver Sirp <silver.sirp@khk.ee>
2022-02-11 01:24:34 +0200

    Update Table

--- db.js
+++ db.js
@@ -10,13 +10,9 @@ var con = mysql.createConnection({
 con.connect(function (err){
     if (err) throw err;
     console.log("Connected!");
-
-
-
-    var sql = "DROP TABLE if exists customers";
-
+    var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log(result);
+        console.log(result.affectedRows + " record(s) updated");
     });
 });
