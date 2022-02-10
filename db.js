8684675293d75df77fbf00d578fae3b16616f156
Silver Sirp <silver.sirp@khk.ee>
2022-02-11 01:15:31 +0200

    Drop Table

--- db.js
+++ db.js
@@ -13,9 +13,9 @@ con.connect(function (err){
 
 
 
-    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    var sql = "DROP TABLE customers";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Number of records deleted: " + result.affectedRows);
+        console.log("Table deleted");
     });
 });
