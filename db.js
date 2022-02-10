158137a4c2e9ab95813feab6bfd4dafa447ba8f5
Silver Sirp <silver.sirp@khk.ee>
2022-02-11 01:29:14 +0200

    Start From Another Position. Shorter Syntax

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 con.connect(function (err){
     if (err) throw err;
     console.log("Connected!");
-    var sql = "SELECT * FROM customers LIMIT 5 OFFSET 2";
+    var sql = "SELECT * FROM customers LIMIT 2, 5";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log(result);
