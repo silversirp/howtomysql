6ffc0dd58f48b23a597594f5d471178ae7228db8
Silver Sirp <silver.sirp@khk.ee>
2022-02-11 01:12:39 +0200

    Delete Record

--- db.js
+++ db.js
@@ -13,8 +13,9 @@ con.connect(function (err){
 
 
 
-    con.query("SELECT * FROM customers ORDER BY name DESC", function (err, result) {
-        if(err) throw err;
-        console.log(result);
+    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Number of records deleted: " + result.affectedRows);
     });
 });
